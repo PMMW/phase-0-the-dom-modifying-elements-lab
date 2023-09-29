c916456fd27fb14b3c7e5841cf40ff5191863890
Morrison Wanja <morrisonwanja@gmail.com>
2023-09-29 10:59:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const toBeremoved = document.getElementById("main")
+main.remove();
+
+
+// const header = document.getElementById("victory")
+const newHeader = document.createElement("h1")
+newHeader.id ="victory";
+newHeader.innerHTML ="YOUR-NAME is the champion"
+document.body.append(newHeader)
+// const newHeader = document.createElement("h1");
+// main.append("newHeader")
